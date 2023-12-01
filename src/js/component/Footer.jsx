e44d778242d119eed8dbaf4378d17a408a0c05cd
Miguel <Megl290304@gmail.com>
2023-12-01 16:39:45 -0400

    commit

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const footer = () => {
+    return(
+        <footer className="mt-5 p-5 bg-dark">
+        <div className="text-center text-white">
+            Copyright©You Website 2023
+        </div>
+      </footer>
+    );
+};
+
+
+
+
+export default footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,40 @@
 import React from "react";
+import Navbar from "./Navbar.jsx";
+import Card from "./Card.jsx";
+import Footer from "./Footer.jsx";
 
-//include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const home = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="container">
+        <div className="p-5 mb-4 bg-light">
+          <div className="container-fluid py-5">
+            <h1 className="display-5 fw-build">A Warm Welcome!</h1>
+            <p className="col-md-8 fs-4">
+              Lorem ipsum dolor, sit amet consectetur adipisicing eli t. Nihil
+              quia, offi cia, dignissimos iste, repudiandae ipsa non fuga soluta
+              ut rem quos quidem ab consectetur vero!
+            </p>
+            <button className="btn btn-primary btn-lg" type="button">
+              Call to action!
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </section>
+
+      <Footer />
+    </>
+  );
 };
 
-export default Home;
+export default home;
